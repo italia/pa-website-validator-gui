1a71ed6f3ed5600cc66e5fe10ef1876f2ca35369
scanavino.marcello <scanavino.marcello@accenture.com>
2024-10-14 15:22:42 +0200

    feat: add stepper.js & fix input style

--- renderer.js
+++ renderer.js
@@ -2,21 +2,19 @@ const { ipcRenderer } = require('electron');
 
 const startBtn = document.getElementById('startBtn');
 const logsTextArea = document.getElementById('logs');
-const iframeContainer = document.getElementById('iframe-container');
+const iframeContainer = document.getElementById('report-container');
 const reportFrame = document.getElementById('reportFrame');
 
 startBtn.addEventListener('click', () => {
-    ipcRenderer.send('start-node-program');
+  ipcRenderer.send('start-node-program');
 });
 
 ipcRenderer.on('log-update', (event, data) => {
-    logsTextArea.value += data + '\n';
-    logsTextArea.scrollTop = logsTextArea.scrollHeight;
+  logsTextArea.value += data + '\n';
+  logsTextArea.scrollTop = logsTextArea.scrollHeight;
 });
 
 ipcRenderer.on('open-report', (event, reportPath) => {
-    reportFrame.src = reportPath;
-    iframeContainer.style.display = 'block';
+  reportFrame.src = reportPath;
+  iframeContainer.style.display = 'block';
 });
-
-
