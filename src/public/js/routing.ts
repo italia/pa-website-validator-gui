e90466012137ae83fc93f32bf821694671241e95
scanavino.marcello <scanavino.marcello@accenture.com>
2024-11-05 10:35:54 +0100

    fix: browser routing

--- src/public/js/routing.ts
+++ src/public/js/routing.ts
@@ -1,11 +1,13 @@
-document.querySelectorAll('a[data-page]').forEach(link => {
-    link.addEventListener('click', event => {        
-        event.preventDefault();
-        
-        if (event && event.target) {
-            //@ts-ignore
-            const pageName = event.target.getAttribute('data-page');
-            window.electronAPI.send('navigate', pageName);
-        }
-    });
+document.querySelectorAll('a[data-page]').forEach((link) => {
+  link.addEventListener('click', (event) => {
+    if (typeof window?.electronAPI?.send === "function") {
+      event.preventDefault();
+
+      if (event && event.target) {
+        //@ts-ignore
+        const pageName = event.target.getAttribute('data-page');
+        window.electronAPI.send('navigate', pageName);
+      }
+    }
+  });
 });
