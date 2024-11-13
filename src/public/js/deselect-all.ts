9cf20aef6072d37413934449c2598d219f168d8c
luca.carrisi <luca.carrisi@ccenture.com>
2024-11-13 17:43:17 +0100

    Feat: Fix funzionalità deseleziona tutto

--- src/public/js/deselect-all.ts
+++ src/public/js/deselect-all.ts
@@ -6,3 +6,12 @@ document.querySelector('[data-deselect-all]')?.addEventListener('click', (e) => 
         (item as HTMLInputElement).checked = false;
     })
 })
+
+document.querySelector('[data-select-all]')?.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    document.querySelectorAll('input[name="audits"]').forEach((item) => {
+        item.setAttribute('checked', '');
+        (item as HTMLInputElement).checked = true;
+    })
+})
